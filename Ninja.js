5f7b858157d6cc3b76017d14d5ae5998769634ca
Andres felipe estrada cortes <afecestra@hotmail.com>
2022-09-25 16:24:14 -0500

    Tearea Sensei

--- Ninja.js
+++ Ninja.js
@@ -18,15 +18,45 @@ class Ninja{
         return ninja1
     }
 
-    srinkSake(){
+    drinkSake(){
         return this.salud +=10;
     }
 
 }
 
+
+class Sensei extends Ninja {
+    
+    constructor(nombre, salud, velocidad, fuerza, sabiduria=10){
+        super(nombre, salud, velocidad, fuerza)
+        this.sabiduria = sabiduria;
+    }
+
+    drinkSake (){
+        return this.salud +=20;
+    }
+
+    speakWisdom (){
+        let padre = super.drinkSake();
+        return padre+" fé en la causa";
+        
+    }
+
+
+}
+
 const ninja1 = new Ninja("andres", 10, 50, 200);
+
+const sensei = new Sensei("felipe", 200, 10,100 );
+
+
+
 
 console.log(ninja1.sayName())
 console.log(ninja1.showStatus())
-console.log(ninja1.srinkSake())
+console.log(ninja1.drinkSake())
 
+console.log(sensei)
+
+console.log(sensei.drinkSake())
+console.log(sensei.speakWisdom())
